9fe0e56a8aabe588294aeb43f0474eea13974788
Dima <dima@cloud66.com>
2018-04-09 13:36:21 +0100

    Speedify

--- app/controllers/hill_controller.rb
+++ app/controllers/hill_controller.rb
@@ -1,15 +1,15 @@
 class HillController < ApplicationController
-  def visit
-	@bucket = Bucket.new
-  	@bucket.size = rand(10)
-  	@bucket.weight = rand(1000)
-  	@bucket.is_full = @bucket.weight > 500
-  	@bucket.save!
-  	@bucket_count = Bucket.all.count
-  end
+	def visit
+		@bucket = Bucket.new
+		@bucket.size = rand(10)
+		@bucket.weight = rand(1000)
+		@bucket.is_full = @bucket.weight > 500
+		@bucket.save!
+		@bucket_count = Bucket.count
+	end
 
-  def read
-  	@bucket_count = Bucket.all.count
-  end
+	def read
+		@bucket_count = Bucket.count
+	end
 
 end
